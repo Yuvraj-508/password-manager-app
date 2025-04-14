c3c5eedc729b3fe8a467ca3b1ceb501db079120a
Vivit Rana <vivitrana@Vivits-MacBook-Air.local>
2025-04-15 01:02:54 +0530

    minor

--- Backend/index.js
+++ Backend/index.js
@@ -45,8 +45,9 @@ app.post("/login", async (req, res) => {
 
 app.post("/register", async (req, res) => {
   const { username, email, password } = req.body;
-  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  User.create({ username, email, password ,ip})
+  const forwarded = req.headers['x-forwarded-for'];
+  const ip = forwarded ? forwarded.split(',')[0].trim() : req.socket.remoteAddress;
+    User.create({ username, email, password ,ip})
     .then((user) => {
       const token = generateToken(user);
       res.json({ user, token });
